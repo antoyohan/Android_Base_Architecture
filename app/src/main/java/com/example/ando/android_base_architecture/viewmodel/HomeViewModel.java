fce0c7de54b42d95590b49b430a51a581b76ecb3
anto.yohan <anto.yohan@DOSYSNET024>
2019-03-08 18:47:51 +0530

    flowable sample

--- app/src/main/java/com/example/ando/android_base_architecture/viewmodel/HomeViewModel.java
+++ app/src/main/java/com/example/ando/android_base_architecture/viewmodel/HomeViewModel.java
@@ -7,14 +7,16 @@ import com.example.ando.android_base_architecture.network.ApiService;
 
 import javax.inject.Inject;
 
+import io.reactivex.BackpressureStrategy;
 import io.reactivex.CompletableObserver;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.MaybeObserver;
-import io.reactivex.Observer;
 import io.reactivex.SingleObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subscribers.DisposableSubscriber;
 
 public class HomeViewModel extends ViewModel {
 
@@ -33,42 +35,6 @@ public class HomeViewModel extends ViewModel {
         flowable();
         //maybe();
         //completable();
-    }
-
-    private void flowable() {
-        PublishSubject<Integer> publishSubject = PublishSubject.create();
-        //publishSubject.toFlowable(BackpressureStrategy.MISSING);
-        publishSubject.subscribeOn(Schedulers.computation()).
-                observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<Integer>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe() called with: d = [" + d + "]");
-            }
-
-            @Override
-            public void onNext(Integer integer) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                Log.d(TAG, "onNext() called with: integer = [" + integer + "]");
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.d(TAG, "onError() called with: e = [" + e + "]");
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "onComplete() called");
-            }
-        });
-        for (int i = 0; i < 10000; i ++) {
-            Log.d(TAG, "emitting " + i);
-            (publishSubject).onNext(i);
-        }
     }
 
     private void maybe() {
@@ -136,6 +102,39 @@ public class HomeViewModel extends ViewModel {
             @Override
             public void onError(Throwable e) {
                 Log.d(TAG, "onError() called with: e = [" + e + "]");
+            }
+        });
+    }
+
+    private void flowable() {
+
+        FlowableOnSubscribe<Integer> flowableOnSubscribe = emitter -> {
+            for (int i = 0; i < 10000; i++) {
+                Log.d(TAG, "emitting: " + i);
+                emitter.onNext(i);
+            }
+        };
+        Flowable<Integer> observable = Flowable.create(flowableOnSubscribe, BackpressureStrategy.LATEST);
+
+        observable.subscribeOn(Schedulers.computation()).observeOn(Schedulers.newThread()).subscribe(new DisposableSubscriber<Integer>() {
+            @Override
+            public void onNext(Integer integer) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Log.d(TAG, "onNext: " + e.getMessage());
+                }
+                Log.d(TAG, "onNext() called with: integer = [" + integer + "]");
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Log.d(TAG, "onError() called with: t = [" + t + "]");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete() called");
             }
         });
     }
